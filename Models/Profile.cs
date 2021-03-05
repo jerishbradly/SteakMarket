f716d0b6329b7415dd0ad82935be85fab41c86d7
Jerish Bradly <jerishbradlyb@gmail.com>
2021-03-05 02:08:35 -0500

    Add project files.

---
+++ Models/Profile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteakMarket.Models
+{
+    public class Profile
+    {
+        public int ProfileId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string SIN { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string MailingAddress { get; set; }
+
+        public int StatusId { get; set; }
+
+        public Status Status { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int PersonalAccountId { get; set; }
+    }
+}
---
+++ Models/SteakMarketContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteakMarket.Models
+{
+    public class SteakMarketContext : DbContext
+    {
+        public SteakMarketContext(DbContextOptions<SteakMarketContext> options) : base(options) { }
+
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
